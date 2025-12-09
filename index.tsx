6658122376aba812aafdd3f060b1a9c3d3eee684
crushmetal <maxime.frouin41@gmail.com>
2025-12-09 16:21:07 +0100

    Refactor index.tsx to use Tailwind CSS and remove React
    Removed React and ReactDOM imports, along with the root rendering logic. Added Tailwind CSS directives and updated CSS styles for the root element.

--- index.tsx
+++ index.tsx
@@ -1,17 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+html, body {
+  height: 100%;
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+#root {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
